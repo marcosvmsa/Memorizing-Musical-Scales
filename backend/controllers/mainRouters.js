95e58ebf364afad66a125bde8431ed913d3dd247
marcosvmsa <marcosvmsa@gmail.com>
2022-05-04 01:04:30 -0700

    attempt for deployment 8

--- backend/controllers/mainRouters.js
+++ backend/controllers/mainRouters.js
@@ -4,10 +4,6 @@ const express = require('express');
 const router = express.Router();
 const Chord = require('../db/models/chords')
 
-
-
-                   
-
 router.get('/:note', (req,res)=>{
     let note = req.params.note 
     Chord.find({note: note})
