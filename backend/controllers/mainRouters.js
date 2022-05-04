74c74136969f8d479e1177491ae97a7db37a234a
marcosvmsa <marcosvmsa@gmail.com>
2022-05-03 20:50:07 -0700

    fixing router

--- backend/controllers/mainRouters.js
+++ backend/controllers/mainRouters.js
@@ -4,6 +4,11 @@ const router = express.Router();
 const Chord = require('../db/models/chords')
 
 
+router.get('/', (req,res)=>{
+    return res.send("Hello World")
+})
+
+
 router.get('/:note', (req,res)=>{
     let note = req.params.note 
     Chord.find({note: note})
