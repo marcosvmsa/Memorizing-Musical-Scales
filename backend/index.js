9ddafbe0156a525c22ed962f75701ca2dda78973
marcosvmsa <marcosvmsa@gmail.com>
2022-05-04 00:43:19 -0700

    attempt for deployment 5

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use('/user',userRouters)
 app.use('/SingUp',signUpRouter)
 
 
-app.get('/app', (req,res)=>{
+app.get('/', (req,res)=>{
     res.sendFile(path.join(__dirname, '/frontend/views/index.html'));
 })
 
