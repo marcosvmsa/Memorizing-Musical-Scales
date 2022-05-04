26362515a7e16d54d356d7773e45dc5dff91bb58
marcosvmsa <marcosvmsa@gmail.com>
2022-05-04 00:46:58 -0700

    attempt for deployment 6

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
-// require('dotenv').config();
-// const express = require('express');
-// const config = require('config');
-// const PORT = process.env.PORT || 4000;
+require('dotenv').config();
+const path = require('path')
+const express = require('express');
+const cors = require('cors');
+const loginRouters = require('./controllers/loginRouters')
+const mainRouters  = require('./controllers/mainRouters')
+const userRouters  = require('./controllers/userRouters')
+const signUpRouter  = require('./controllers/signUpRouter')
+const methodOverride = require('method-override')
+const PORT = process.env.PORT || 4000;
+const app  = express();
 
-// const app  = express();
-// const cors = require('cors');
-// const methodOverride = require('method-override')
-// const loginRouters = require('./backend/controllers/loginRouters')
-// const mainRouters  = require('./backend/controllers/mainRouters')
-// const userRouters  = require('./backend/controllers/userRouters')
-// const signUpRouter  = require('./backend/controllers/signUpRouter')
+app.use(express.json());
+app.use(methodOverride('_method'))
+app.use(express.urlencoded({extended: true}));
+app.use(cors())
+app.use('/', express.static(path.join(__dirname, '/frontend/views/index.html')))
+app.use('/',mainRouters);
+app.use('/login',loginRouters)
+app.use('/user',userRouters)
+app.use('/SingUp',signUpRouter)
 
-// app.use(express.json());
-// app.use(methodOverride('_method'))
-// app.use(express.urlencoded({extended: true}));
-// app.use(cors())
-// // app.use('/', express.static(path.join(__dirname, 'frontend')))
-// app.use('/',mainRouters);
-// app.use('/login',loginRouters)
-// app.use('/user',userRouters)
-// app.use('/SingUp',signUpRouter)
 
-// app.listen(PORT, ()=>{
-//     console.log(`Runing on Port :  ${PORT}`)
-// })
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/frontend/views/index.html'));
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Runing on Port :  ${PORT}`)
+})
+
+
